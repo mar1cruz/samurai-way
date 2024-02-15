3072c72273212808986c12a4e2988397ac2cb287
dmitry <ddovidovich@list.ru>
2024-02-15 19:38:26 +0300

    add module.css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import s from './Header.module.css'
 
 const Header = () => {
     return (
-        <header className='header'>
-            <img className='header-img'
-                 src="https://www.trashedgraphics.com/wp-content/uploads/2013/03/vector_fish_logo.jpg" alt=""/>
+        <header className={s.header}>
+            <img className={s.headerImg}
+                src="https://www.trashedgraphics.com/wp-content/uploads/2013/03/vector_fish_logo.jpg" alt=""/>
         </header>
     );
 };
