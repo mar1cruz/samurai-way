e46079a12e78825e6b0990dbef1bb19d923eaf21
dmitry <ddovidovich@list.ru>
2024-02-28 15:40:39 +0300

    add Navlinks for Route

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div className={s.item + ' ' + s.active}><a href="/profile">Profile</a></div>
-            <div className={s.item}><a href="/dialogs">Messages</a></div>
-            <div className={s.item}><a href="/news">News</a></div>
-            <div className={s.item}><a href="/music">Music</a></div>
-            <div className={s.item}><a href="/settings">Settings</a></div>
+            <div className={s.item}>
+                <NavLink activeClassName={s.activeLink} to="/profile">Profile</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink activeClassName={s.activeLink} to="/dialogs">Messages</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink activeClassName={s.activeLink} to="/news">News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink activeClassName={s.activeLink} to="/music">Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink activeClassName={s.activeLink} to="/settings">Settings</NavLink>
+            </div>
         </nav>
     );
 };
