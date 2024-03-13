253c12656f46a37215d9a93d77808376b22e8758
dmitry <ddovidovich@list.ru>
2024-03-13 21:23:28 +0300

    brought out the data

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+export type DialogsType = {
+    id: number
+    name: string
+}
+
+const dialogs: DialogsType[] = [
+    {id: 1, name: 'Dimych'},
+    {id: 2, name: 'Andrey'},
+    {id: 3, name: 'Sveta'},
+    {id: 4, name: 'Sasha'},
+    {id: 5, name: 'Valera'},
+    {id: 6, name: 'Viktor'},
+]
+
+export type MessageType = {
+    id: number,
+    message: string
+}
+
+const messages: MessageType[] = [
+    {id: 1, message: 'Hi'},
+    {id: 2, message: 'How are you?'},
+    {id: 3, message: 'Hello'},
+    {id: 4, message: 'Yo'},
+    {id: 5, message: 'Yo'},
+    {id: 6, message: 'Yo'},
+]
+
+export type PostType = {
+    id: number
+    likesCount: number
+    message: string
+}
+
+const postData: PostType[] = [
+    {id: 1, message: 'Hi, how are you', likesCount: 12},
+    {id: 2, message: "It's my first post", likesCount: 11},
+]
+
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+    <App dialogsData={dialogs} messageData={messages} postData={postData}/>,
+    document.getElementById('root')
 );
